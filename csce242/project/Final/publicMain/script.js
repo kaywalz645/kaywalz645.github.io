f022f528d1162a1f028c956199fec01f47a90307
kaywalz645 <kaybalz645@gmail.com>
2023-12-12 19:27:54 -0500

    mostly working

--- csce242/project/Final/publicMain/script.js
+++ csce242/project/Final/publicMain/script.js
@@ -14,19 +14,14 @@ const toggleHamMenu = () => {
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the navigation links
     const navigationLinks = document.querySelectorAll('a');
 
-    // Add click event listeners to each navigation link
     navigationLinks.forEach(link => {
         link.addEventListener('click', function (event) {
-            // Prevent the default behavior of the link (e.g., following the href)
             event.preventDefault();
 
-            // Get the href attribute of the clicked link
             const targetPage = this.getAttribute('href');
 
-            // Change the current page to the target page
             window.location.href = targetPage;
         });
     });
--- csce242/project/Final/server.js
+++ csce242/project/Final/server.js
@@ -1,19 +1,155 @@
-// server.js
+
+
 const express = require("express");
 const app = express();
 const multer = require("multer");
 const path = require('path');
-app.use(express.static("publicMain"));
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+
+mongoose.connect("mongodb+srv://kaybalz645:dreamSMP645@kaylee.ixxu8d8.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Couldn't connect to MongoDB", error);
   });
 
-  
-//Set the "final" folder as the static content root
-app.use(express.static(path.join(__dirname, 'Final')));
 
-//Method to serve index.html file for a specific folder
+const jobSchema = new mongoose.Schema({
+  //_id: mongoose.SchemaTypes.ObjectId,
+  name: String,
+  description: String,
+});
+
+const Job = mongoose.model("Job", jobSchema, "test.jobs");
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(cors());
+
+const upload = multer({ dest: __dirname + "/public/images" });
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/jobs", (req, res) => {
+  getJobs(res);
+});
+
+app.get("/api/jobs/:id", (req, res) => {
+  getJob(req.params.id, res);
+});
+
+app.post("/api/jobs", (req, res) => {
+  const result = validateJob(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const job = new Job({
+    name: req.body.name,
+    description: req.body.description,
+  });
+
+  createJob(job, res);
+});
+
+app.put("/api/jobs/:id", (req, res) => {
+  const result = validateJob(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  updateJob(req, res);
+});
+
+app.delete("/api/jobs/:id", (req, res) => {
+  removeJob(res, req.params.id);
+});
+const removeJob = async (res, id) => {
+  const job = await Job.findByIdAndDelete(id);
+  res.send(job);
+};
+
+const getJobs = async (res) => {
+  try {
+    const jobs = await Job.find();
+    res.send(jobs);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+async function getJob(id, res) {
+  const job = await Job.findOne({ _id: id });
+  res.send(job);
+}
+
+function createJob(job, res) {
+  job.save()
+  .then(savedJob => {
+    res.send(savedJob);
+  })
+  .catch(err => {
+    console.error('Error saving job:', err);
+    res.status(500).send("Internal Server Error");
+  });
+}
+
+function updateJob(req, res) {
+  let fieldsToUpdate = {
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  Job.findByIdAndUpdate(req.params.id, fieldsToUpdate, { new: true }, (err, updatedJob) => {
+    if (err) {
+      console.error('Error updating job:', err);
+      res.status(500).send("Internal Server Error");
+    } else if (!updatedJob) {
+      res.status(404).send("Job not found");
+    } else {
+      res.send(updatedJob);
+    }
+  });
+}
+
+// function removeJob(res, id) {
+//   Job.findByIdAndDelete(id, (err, deletedJob) => {
+//     if (err) {
+//       console.error('Error deleting job:', err);
+//       res.status(500).send("Internal Server Error");
+//     } else if (!deletedJob) {
+//       res.status(404).send("Job not found");
+//     } else {
+//       res.send(deletedJob);
+//     }
+//   });
+// }
+
+function validateJob(job) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    description: Joi.string().min(3).required(),
+    // _id: Joi.allow(""),
+  });
+
+  return schema.validate(job);
+}
+
+
+app.use(express.static("publicMain"));
+
+app.get('/joinOurTeam', serveFolder('joinOurTeam'));
+
+app.get('/contactUs', serveFolder('contactUs'));
+
 function serveFolder(folderName) {
   return (req, res) => {
     const filePath = path.join(__dirname, 'Final', folderName, 'index.html');
@@ -21,32 +157,11 @@ function serveFolder(folderName) {
   };
 }
 
-// // Method to serve styles.css file for a specific folder
-function serveStyles(folderName) {
-  return (req, res) => {
-    const filePath = path.join(__dirname, 'Final', folderName, 'styles.css');
-    res.sendFile(filePath);
-  };
-}
 
-// // Method to serve script.js file for a specific folder
-function serveScript(folderName) {
-  return (req, res) => {
-    const filePath = path.join(__dirname, 'Final', folderName, 'script.js');
-    res.sendFile(filePath);
-  };
-}
-
-//noSet up routes for each folder
-app.get('/joinOurTeam', serveFolder('joinOurTeam'));
-app.get('/joinOurTeam/styles.css', serveStyles('joinOurTeam'));
-app.get('/joinOurTeam/script.js', serveScript('joinOurTeam'));
-
-app.get('contactUs', serveFolder('contactUs'));
-app.get('contactUs/styles.css', serveStyles('contactUs'));
-app.get('contactUs/script.js', serveScript('contactUs'));
-
-// Start the server
 app.listen(3000, () => {
-  console.log(`Listening`);
+  console.log("Server is listening on port 3000");
 });
+
+
+
+ 
