6cdc51acc9480a54a9dfa33af03b0ad82e27c279
kaywalz645 <kaybalz645@gmail.com>
2025-02-18 00:17:02 -0500

    cleaner look, not cleaner code lol

--- server.js
+++ server.js
@@ -1,129 +1,139 @@
 const express = require("express");
 const app = express();
-const multer = require("multer");
-const Joi = require("joi");
-const path = require('path');
-// app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-const cors = require("cors");
-app.use(cors());
-const mongoose = require("mongoose");
+const PORT = 3000;
 
-const upload = multer({ dest: __dirname + "/public/images" });
+app.use(express.static("public")); // Serve static files from the "public" folder
 
-mongoose
-  .connect("mongodb+srv://kaybalz645:5QQ4tJZKqN.3tKy@kaylee.ixxu8d8.mongodb.net/?retryWrites=true&w=majority")
-  .then(() => {
-    console.log("connected to mongodb");
-  })
-  .catch((error) => {
-    console.log("couldn't connect to mongodb", error);
-  });
-
-const jobSchema = new mongoose.Schema({
-  _id: mongoose.SchemaTypes.ObjectId,
-  name: String,
-  description: String,
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
 
-const Job = mongoose.model("jobs", jobSchema);
+// const express = require("express");
+// const app = express();
+// const multer = require("multer");
+// const Joi = require("joi");
+// const path = require('path');
+// // app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// const cors = require("cors");
+// app.use(cors());
+// const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// const upload = multer({ dest: __dirname + "/public/images" });
 
-app.get("/api/jobs", (req, res) => {
-  console.log("got here");
-  getJobs(res);
-});
+// mongoose
+//   .connect("mongodb+srv://kaybalz645:5QQ4tJZKqN.3tKy@kaylee.ixxu8d8.mongodb.net/?retryWrites=true&w=majority")
+//   .then(() => {
+//     console.log("connected to mongodb");
+//   })
+//   .catch((error) => {
+//     console.log("couldn't connect to mongodb", error);
+//   });
 
-const getJobs = async (res) => {
-  const jobs = await Job.find();
-  res.send(jobs);
-};
+// const jobSchema = new mongoose.Schema({
+//   _id: mongoose.SchemaTypes.ObjectId,
+//   name: String,
+//   description: String,
+// });
 
-app.get("/api/jobs/:id", (req, res) => {
-  getJob(req.params.id, res);
-});
+// const Job = mongoose.model("jobs", jobSchema);
 
-const getJob = async (id, res) => {
-  const job = await Job.findOne({ _id: id });
-  res.send(job);
-};
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 
-app.post("/api/jobs", (req, res) => {
-  const result = validateJob(req.body);
-  console.log(result);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+// app.get("/api/jobs", (req, res) => {
+//   console.log("got here");
+//   getJobs(res);
+// });
 
-  const job = new Job({
-    name: req.body.name,
-    description: req.body.description,
-  });
+// const getJobs = async (res) => {
+//   const jobs = await Job.find();
+//   res.send(jobs);
+// };
 
-  createJob(job, res);
-});
+// app.get("/api/jobs/:id", (req, res) => {
+//   getJob(req.params.id, res);
+// });
 
-const createJob = async (job, res) => {
-  const result = await job.save();
-  res.send(job);
-};
+// const getJob = async (id, res) => {
+//   const job = await Job.findOne({ _id: id });
+//   res.send(job);
+// };
 
-app.put("/api/jobs/:id", (req, res) => {
-  const result = validateJob(req.body);
-  console.log(result);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-  updateJob(req, res);
-});
+// app.post("/api/jobs", (req, res) => {
+//   const result = validateJob(req.body);
+//   console.log(result);
+//   if (result.error) {
+//     res.status(400).send(result.error.details[0].message);
+//     return;
+//   }
 
-const updateJob = async (req, res) => {
-  let fieldsToUpdate = {
-    name: req.body.name,
-    description: req.body.description,
-  };
+//   const job = new Job({
+//     name: req.body.name,
+//     description: req.body.description,
+//   });
 
-  const result = await Job.updateOne({ _id: req.params.id }, fieldsToUpdate);
+//   createJob(job, res);
+// });
 
-  res.send(result);
-};
+// const createJob = async (job, res) => {
+//   const result = await job.save();
+//   res.send(job);
+// };
 
-app.delete("/api/jobs/:id", (req, res) => {
-  removeJob(res, req.params.id);
-});
+// app.put("/api/jobs/:id", (req, res) => {
+//   const result = validateJob(req.body);
+//   console.log(result);
+//   if (result.error) {
+//     res.status(400).send(result.error.details[0].message);
+//     return;
+//   }
+//   updateJob(req, res);
+// });
 
-const removeJob = async (res, id) => {
-  try {
-    console.log('Deleting job with ID:', id);
+// const updateJob = async (req, res) => {
+//   let fieldsToUpdate = {
+//     name: req.body.name,
+//     description: req.body.description,
+//   };
 
-    const deletedJob = await Job.findByIdAndDelete(id);
-    console.log('Deleted job:', deletedJob);
-    if (!deletedJob) {
-      res.status(404).send("Job not found");
-    } else {
-      res.send(deletedJob);
-    }
-  } catch (err) {
-    console.error('Error deleting job:', err);
-    res.status(500).send("Internal Server Error");
-  }
-};
+//   const result = await Job.updateOne({ _id: req.params.id }, fieldsToUpdate);
 
-function validateJob(job) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-    description: Joi.string().min(3).required(),
-  });
+//   res.send(result);
+// };
 
-  return schema.validate(job);
-}
+// app.delete("/api/jobs/:id", (req, res) => {
+//   removeJob(res, req.params.id);
+// });
 
-app.listen(3000, () => {
-  console.log("I'm listening");
-});
+// const removeJob = async (res, id) => {
+//   try {
+//     console.log('Deleting job with ID:', id);
+
+//     const deletedJob = await Job.findByIdAndDelete(id);
+//     console.log('Deleted job:', deletedJob);
+//     if (!deletedJob) {
+//       res.status(404).send("Job not found");
+//     } else {
+//       res.send(deletedJob);
+//     }
+//   } catch (err) {
+//     console.error('Error deleting job:', err);
+//     res.status(500).send("Internal Server Error");
+//   }
+// };
+
+// function validateJob(job) {
+//   const schema = Joi.object({
+//     name: Joi.string().min(3).required(),
+//     description: Joi.string().min(3).required(),
+//   });
+
+//   return schema.validate(job);
+// }
+
+// app.listen(3000, () => {
+//   console.log("I'm listening");
+// });
